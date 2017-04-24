156593eb52897eb3ed9a772d6c201edc1d4f89f7
Christoffer Bo Petersen <cb.petersen@gmail.com>
2017-04-24 14:52:17 +0200

    :sparkles: Add generation of flow type definitions

---
+++ src/flow-type-generator.js
@@ -0,0 +1,50 @@
+
+import { indent, append } from './utils'
+import { Field, Schema, Message, primitiveTypes, ProtoToJsTypeMapping } from './types'
+
+export const generateFlowType = (name: string, fields: string) =>
+`
+export type ${name} = {
+${fields}
+}
+`.trim()
+
+export const generateField = (field: Field, indention, index, length) => {
+  if (primitiveTypes.some(type => type === field.type)) {
+    if (field.repeated) {
+      return `${indent(indention)}${field.name}: Array<${ProtoToJsTypeMapping[field.type]}>${append(index, length)}`
+    }
+
+    return `${indent(indention)}${field.name}: ${ProtoToJsTypeMapping[field.type]}${append(index, length)}`
+  }
+
+  if (field.repeated) {
+    return `${indent(indention)}${field.name}: Array<${field.type}>${append(index, length)}`
+  }
+
+  return `${indent(indention)}${field.name}: ${field.type}${append(index, length)}`
+}
+
+export const messageMapper = (message: Message) => {
+  const output = []
+  const indention = 2
+
+  message.fields.forEach((field, innerIndex) => {
+    output.push(generateField(field, indention, innerIndex, message.fields.length))
+  }, this)
+
+  return generateFlowType(message.name, output.join('\n'))
+}
+
+export const generateFileHeader = () => `// @flow`.trim()
+
+export default (schema: Schema) => {
+  const output = []
+
+  output.push(generateFileHeader())
+  schema.messages.forEach((message) => {
+    output.push(messageMapper(message))
+  }, this)
+
+  return output.join('\n\n')
+}
---
+++ src/test/flow-type-generator.spec.js
@@ -0,0 +1,113 @@
+/* eslint-env jest */
+
+import generator, { generateFlowType, generateField, messageMapper, generateFileHeader } from '../flow-type-generator'
+
+describe('generateFlowType', () => {
+  it('creates an empty flow type definition', () => {
+    const rsp = generateFlowType('Empty', '')
+
+    expect(rsp).toMatchSnapshot()
+  })
+
+  it('creates an flow type definition', () => {
+    const rsp = generateFlowType('Empty', '  name: string')
+
+    expect(rsp).toMatchSnapshot()
+  })
+})
+
+describe('generateField', () => {
+  it('creates a primitive value definition', () => {
+    let field = {
+      type: 'string',
+      name: 'addressString'
+    }
+    const rsp = generateField(field, 2, 1, 1)
+
+    expect(rsp).toMatchSnapshot()
+  })
+
+  it('creates a repeated primitive value definition', () => {
+    let field = {
+      type: 'string',
+      name: 'addressStrings',
+      repeated: true
+    }
+    const rsp = generateField(field, 2, 1, 1)
+
+    expect(rsp).toMatchSnapshot()
+  })
+
+  it('creates a type definition', () => {
+    let field = {
+      type: 'Location',
+      name: 'Place'
+    }
+    const rsp = generateField(field, 2, 1, 1)
+
+    expect(rsp).toMatchSnapshot()
+  })
+
+  it('creates a repeated type definition', () => {
+    let field = {
+      type: 'Location',
+      name: 'Places',
+      repeated: true
+    }
+    const rsp = generateField(field, 2, 1, 1)
+
+    expect(rsp).toMatchSnapshot()
+  })
+})
+
+describe('messageMapper', () => {
+  it('creates a flow type definition', () => {
+    let message = {
+      name: 'Location',
+      fields: [{
+        type: 'string',
+        name: 'addressString'
+      }, {
+        type: 'float',
+        name: 'lat'
+      }, {
+        type: 'float',
+        name: 'lng'
+      }]
+    }
+    const rsp = messageMapper(message)
+
+    expect(rsp).toMatchSnapshot()
+  })
+})
+
+describe('generator', () => {
+  it('creates a hole flow type definition file output', () => {
+    let schema = {
+      messages: [{
+        name: 'Location',
+        fields: [{
+          type: 'string',
+          name: 'addressString'
+        }, {
+          type: 'float',
+          name: 'lat'
+        }, {
+          type: 'float',
+          name: 'lng'
+        }]
+      }, {
+        name: 'Places',
+        fields: [{
+          type: 'Location',
+          name: 'locations',
+          repeated: true
+        }]
+      }]
+    }
+    const rsp = generator(schema)
+
+    expect(rsp).toMatchSnapshot()
+  })
+})
+
