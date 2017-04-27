2fb05ea07600fd0fc7367b9fa33a9c22b0e90bbe
Christoffer Bo Petersen <cb.petersen@gmail.com>
2017-04-27 11:20:21 +0200

    :sparkles: Add ability to process directories

--- src/fileHandler.js
+++ src/fileHandler.js
@@ -16,31 +16,41 @@ import indexGen from './js-generators/index-generator'
 
 import { fileName, jsFileName, objcClassPrefix } from './utils'
 
-export default (protoFile: string) => {
-  const file = fs.readFileSync(protoFile, 'utf-8')
+const processFile = (filePathString: string) => {
+  const file = fs.readFileSync(filePathString, 'utf-8')
   const schema = schemaParser.parse(file)
-  const realProtoFileName = path.parse(protoFile).name
+  const realProtoFileName = path.parse(filePathString).name
   const protoFileName = schema.services[0].name
   const iosBridgeFile = ios(schema, realProtoFileName)
   const iosBridgeHeaderFile = iosHeaderCreator(protoFileName)
 
-  if (!fs.existsSync('output')) {
-    fs.mkdirSync('output')
-  } else {
-    del.sync(['output/**'])
-    fs.mkdirSync('output')
-    fs.mkdirSync('output/js')
-    fs.mkdirSync(`output/js/${jsFileName(protoFileName)}`)
-    fs.mkdirSync('output/ios')
-  }
+  fs.mkdirSync(`output/js/${jsFileName(realProtoFileName)}`)
 
   fs.writeFileSync(`output/ios/${objcClassPrefix(schema)}${fileName(protoFileName)}.h`, iosBridgeHeaderFile, {encoding: 'utf-8'})
   fs.writeFileSync(`output/ios/${objcClassPrefix(schema)}${fileName(protoFileName)}.m`, iosBridgeFile, {encoding: 'utf-8'})
-  fs.writeFileSync(`output/js/${jsFileName(protoFileName)}/flow-types.js`, flowTypes(schema), {encoding: 'utf-8'})
-  fs.writeFileSync(`output/js/${jsFileName(protoFileName)}/actions.js`, reactActionCreators(schema), {encoding: 'utf-8'})
-  fs.writeFileSync(`output/js/${jsFileName(protoFileName)}/action-types.js`, actionTypes(schema), {encoding: 'utf-8'})
-  fs.writeFileSync(`output/js/${jsFileName(protoFileName)}/api-client.js`, apiClient(schema), {encoding: 'utf-8'})
-  fs.writeFileSync(`output/js/${jsFileName(protoFileName)}/index.js`, indexGen(schema), {encoding: 'utf-8'})
+  fs.writeFileSync(`output/js/${jsFileName(realProtoFileName)}/flow-types.js`, flowTypes(schema), {encoding: 'utf-8'})
+  fs.writeFileSync(`output/js/${jsFileName(realProtoFileName)}/actions.js`, reactActionCreators(schema), {encoding: 'utf-8'})
+  fs.writeFileSync(`output/js/${jsFileName(realProtoFileName)}/action-types.js`, actionTypes(schema), {encoding: 'utf-8'})
+  fs.writeFileSync(`output/js/${jsFileName(realProtoFileName)}/api-client.js`, apiClient(schema), {encoding: 'utf-8'})
+  fs.writeFileSync(`output/js/${jsFileName(realProtoFileName)}/index.js`, indexGen(schema), {encoding: 'utf-8'})
+}
 
-  console.log(iosBridgeFile)
+const processDirectory = (directoryPathString:string) => {
+  const files = fs.readdirSync(directoryPathString)
+  files.forEach(file => {
+    processFile(path.join(directoryPathString, file))
+  })
+}
+
+export default (pathString: string) => {
+  del.sync(['output/**'])
+  fs.mkdirSync('output')
+  fs.mkdirSync('output/ios')
+  fs.mkdirSync('output/js')
+
+  if (fs.lstatSync(pathString).isDirectory()) {
+    return processDirectory(pathString)
+  }
+
+  return processFile(pathString)
 }
